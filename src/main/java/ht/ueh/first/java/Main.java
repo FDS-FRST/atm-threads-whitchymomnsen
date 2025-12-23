229192bcdf2b5c9afc0eb575e4d2b53cdd31e905
Deyizark <mmomnsen@gmail.com>
2025-12-23 13:31:36 -0500

    --Implementation Simulation multi-clients

--- src/main/java/ht/ueh/first/java/Main.java
+++ src/main/java/ht/ueh/first/java/Main.java
@@ -1,22 +1,50 @@
 package ht.ueh.first.java;
 
 import ht.ueh.first.java.enums.ActionType;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         BankAccount account = new BankAccount(1, 1000);
-        account.deposit(200);
-        account.withdraw(150);
-        System.out.println("Solde final : " + account.getBalance() + " €");
+//        account.deposit(200);
+//        account.withdraw(150);
+//        System.out.println("Solde final : " + account.getBalance() + " €");
+//
+//        Thread t1 = new Thread(new ATMTask(account, ActionType.WITHDRAW, 700), "Client-1");
+//        Thread t2 = new Thread(new ATMTask(account, ActionType.WITHDRAW, 700), "Client-2");
+//
+//        t1.start();
+//        t2.start();
+//        Thread.sleep(5000);
+//        System.out.println("Solde final : " + account.getBalance() + " €");
 
-        Thread t1 = new Thread(new ATMTask(account, ActionType.WITHDRAW, 700), "Client-1");
-        Thread t2 = new Thread(new ATMTask(account, ActionType.WITHDRAW, 700), "Client-2");
 
-        t1.start();
-        t2.start();
-        Thread.sleep(5000);
-        System.out.println("Solde final : " + account.getBalance() + " €");
+        Random random = new Random();
+
+        for (int i = 1; i <= 10; i++) {
+            Thread client = new Thread(() -> {
+                for (int j = 0; j < 5; j++) {
+                    int choice = random.nextInt(3);
+                    double amount = random.nextInt(500) + 1;
+
+                    try {
+                        switch (choice) {
+                            case 0 -> account.deposit(amount);
+                            case 1 -> account.withdraw(amount);
+                            case 2 -> System.out.println(
+                                    Thread.currentThread().getName() +
+                                            " consulte solde = " + account.getBalance());
+                        }
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, "Client-" + i);
+
+            client.start();
+        }
 
     }
 }
