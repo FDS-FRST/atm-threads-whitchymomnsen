40a8d5e6eb40fe5a7fde42ad281916f6774aa86f
Deyizark <mmomnsen@gmail.com>
2025-12-23 13:46:32 -0500

    --Implementation des Exceptions

---
+++ src/main/java/ht/ueh/first/java/InsufficientFundsException.java
@@ -0,0 +1,7 @@
+package ht.ueh.first.java;
+
+public class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ht/ueh/first/java/InvalidAmountException.java
@@ -0,0 +1,7 @@
+package ht.ueh.first.java;
+
+public class InvalidAmountException extends RuntimeException {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
