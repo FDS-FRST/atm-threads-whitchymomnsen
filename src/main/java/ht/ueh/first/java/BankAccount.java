6ff7d5c8fe327ee659bb0bbc40b471d236e6f4c9
whaugutin <whitchyaugustin@gmail.com>
2025-12-23 12:12:36 -0500

    Testing new BankAccount, a deposit and aw withdraw

--- src/main/java/ht/ueh/first/java/BankAccount.java
+++ src/main/java/ht/ueh/first/java/BankAccount.java
@@ -4,6 +4,11 @@ public class BankAccount {
     private int accountNumber;
     private double balance;
 
+    public BankAccount(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
     public void deposit(double amount) {
         this.balance += amount;
     }
--- src/main/java/ht/ueh/first/java/Main.java
+++ src/main/java/ht/ueh/first/java/Main.java
@@ -2,7 +2,12 @@ package ht.ueh.first.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-        System.out.println("Please see the readme for your assignation");
+
+        BankAccount account = new BankAccount(1, 1000);
+
+        account.deposit(200);
+        account.withdraw(150);
+
+        System.out.println("Solde final : " + account.getBalance() + " €");
     }
 }
